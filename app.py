2ccaa03f50c0252e97a95a596b731550cb7ae209
mlabenski <mlabenski123@gmail.com>
2020-04-15 22:03:58 -0700

    trying something new

--- app.py
+++ app.py
@@ -9,32 +9,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    covid_data = requests.get('https://covid2019-api.herokuapp.com/v2/current')
-    covid_data = covid_data.json()
-    covid_data = pd.DataFrame.from_dict(covid_data['data'])
-    covid_data_max = covid_data['confirmed'].max()
-    covid_data_max = covid_data_max.item()
-
-    world_geo = r'countries.geojson'
-    world_map = folium.Map(location=[4.68, 8.33],
-                        tiles='Mapbox Bright', zoom_start=3)
-    world_map = folium.Choropleth(
-        geo_data=world_geo,
-        name='choropleth',
-        data=covid_data,
-        columns=['location','confirmed'],
-        key_on='properties.ADMIN',
-        threshold_scale = [0,int((covid_data_max/15)),int((covid_data_max/10)),int((covid_data_max/4)),covid_data_max],
-        fill_color='BuPu',
-        fill_opacity=0.7,
-        line_opacity=0.2,
-        legend_name='Number of deaths per country',
-        highlight=True,
-        line_color='black'
-        ).add_to(world_map)
-
-    folium.LayerControl().add_to(world_map)
-    world_map.save('templates/map.html')
     return render_template('index.html')
 
 @app.route('/map')
