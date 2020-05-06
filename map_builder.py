8e8dc3eb9a96480db84f07ce603f199a3fb79124
mlabenski <mlabenski123@gmail.com>
2020-05-06 10:51:04 -0700

    python suppliment of flask project

--- map_builder.py
+++ map_builder.py
@@ -11,9 +11,18 @@ import numpy as np
 
 covid_data = requests.get('https://covid2019-api.herokuapp.com/v2/current')
 covid_data = covid_data.json()
+
+for data, values in covid_data.items():
+    if data == 'data':
+        for country in values:
+            if country['location'] == 'US':
+                country['location']='United States of America'
+                print(country['location'])
 covid_data = pd.DataFrame.from_dict(covid_data['data'])
+#covid_data.location = covid_data.location.replace('US', "United States of America")
 covid_data_max = covid_data['confirmed'].max()
 covid_data_max = covid_data_max.item()
+
 
 world_geo = r'countries.geojson'
 world_map = folium.Map(location=[4.68, 8.33],
@@ -24,11 +33,11 @@ world_map = folium.Choropleth(
     data=covid_data,
     columns=['location','confirmed'],
     key_on='properties.ADMIN',
-    threshold_scale = [0,int((covid_data_max/15)),int((covid_data_max/10)),int((covid_data_max/4)),covid_data_max],
+    threshold_scale = [0,100,(covid_data_max/10),(covid_data_max/4),covid_data_max],
     fill_color='BuPu',
     fill_opacity=0.7,
     line_opacity=0.2,
-    legend_name='Number of deaths per country',
+    legend_name='Number of cases per country',
     highlight=True,
     line_color='black'
     ).add_to(world_map)
